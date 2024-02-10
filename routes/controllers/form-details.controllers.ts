5f5a8fa26ec242f7c28942dd9fa8c0b9e31d73e5
HueHueberry <huby.new1112@gmail.com>
2024-02-10 22:05:48 +0800

    updated form request

--- routes/controllers/form-details.controllers.ts
+++ routes/controllers/form-details.controllers.ts
@@ -2,35 +2,15 @@ import { Request, Response } from "express";
 import { requestHandler } from "../utils";
 import * as service from "../services/form-details.service";
 
-export const setDean = (req: Request, res: Response) => {
+export const updateAll = (req: Request, res: Response) => {
   requestHandler(res, () => {
-    const { dean } = req.body;
+    const { dean, vcaa, academic_year, semester } = req.body;
     service.setDean(dean);
-    res.json({ created: true });
-  });
-};
-
-export const setVCAA = (req: Request, res: Response) => {
-  requestHandler(res, () => {
-    const { vcaa } = req.body;
-    service.setVCAA(vcaa);
-    res.json({ created: true });
-  });
-};
-
-export const setSemester = (req: Request, res: Response) => {
-  requestHandler(res, () => {
-    const { semester } = req.body;
-    service.setSemester(semester);
-    res.json({ created: true });
-  });
-};
-
-export const setAcademicYear = (req: Request, res: Response) => {
-  requestHandler(res, () => {
-    const { academic_year } = req.body;
     service.setAcademicYear(academic_year);
-    res.json({ created: true });
+    service.setVCAA(vcaa);
+    service.setSemester(semester);
+
+    res.json({ updated: true });
   });
 };
 
--- routes/form.ts
+++ routes/form.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { getAll, setDean, setAcademicYear, setSemester, setVCAA } from "./controllers/form-details.controllers";
+import { getAll, updateAll } from "./controllers/form-details.controllers";
 
 const router = Router();
 
@@ -7,20 +7,8 @@ router.get('/', (req: Request, res: Response) => {
   getAll(res);
 });
 
-router.post('/dean', (req: Request, res: Response) => {
-  setDean(req, res);
-});
-
-router.post('/ay', (req: Request, res: Response) => {
-  setAcademicYear(req, res);
-});
-
-router.post('/semester', (req: Request, res: Response) => {
-  setSemester(req, res);
-});
-
-router.post('/vcaa', (req: Request, res: Response) => {
-  setVCAA(req, res);
+router.post('/', (req: Request, res: Response) => {
+  updateAll(req, res);
 });
 
 export default router;
